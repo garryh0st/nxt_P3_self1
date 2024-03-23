8b9634caa5cd40cb31a6336619f6fcb52c834a8e
anonymous-user02 <advikaggarwal02@gmail.com>
2024-03-23 22:40:19 +0530

    update

---
+++ app/(main)/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/nav/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="  h-screen w-screen bg-amber-500 items-end">
+      <div className=" ">
+        <p className="">some heading type shit </p>
+      </div>
+    </div>
+  );
+}
