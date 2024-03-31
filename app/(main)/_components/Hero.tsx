e403a6b4c4a21266400c7167d5ca3f21a0466657
anonymous-user02 <advikaggarwal02@gmail.com>
2024-03-31 22:13:09 +0530

    nav items edit

--- app/(main)/_components/Hero.tsx
+++ app/(main)/_components/Hero.tsx
@@ -4,14 +4,20 @@ const Hero = () => {
   return (
     <>
       <section>
-        <div className="h-screen w-screen  fixed ">
+        <div className="h-screen w-screen  fixed bg-[#111111] ">
           <nav className="flex justify-between mt-10 paragraph">
-            <div className="nav_left flex justify-between">
-              <span>Advik Aggrwal</span>
-              <span>Web Developer</span>
+            <div className="nav_left flex justify-between mr-[50vh] open-font">
+              <span className="text-[#aaaaaa]">Advik Aggarwal</span>
+              <span className="text-[#777777]">
+                Web Developer
+                <br />
+                /2018─2024
+              </span>
             </div>
-            <div className="nav_right flex justify-between">
-              <span>Advik Aggrwal</span>
+            <div className="nav_right flex justify-around open-font">
+              <span className="text-[#777777] text-wrap h-20 w-[20vh] text-[1.6vh]">
+                AVAILABLE FOR FREELANCE WORK FROM JUNE 2024
+              </span>
               <span>Web Developer</span>
             </div>
           </nav>
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-[#111111]">
+      <body>
         <div>
           <SmoothScrolling>{children}</SmoothScrolling>
         </div>
