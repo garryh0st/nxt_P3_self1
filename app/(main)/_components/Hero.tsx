810c09f64ee81db3a329e7cecfa42eb7f2ce10cf
anonymous-user02 <advikaggarwal02@gmail.com>
2024-03-31 21:57:44 +0530

    nav items edit

--- app/(main)/_components/Hero.tsx
+++ app/(main)/_components/Hero.tsx
@@ -4,12 +4,20 @@ const Hero = () => {
   return (
     <>
       <section>
-        <div className="h-screen w-screen bg-[#111111]">
-          <div className="flex justify-between">
-            <span>Advik Aggrwal</span>
-            <span>Web Developer</span>
+        <div className="h-screen w-screen  fixed ">
+          <nav className="flex justify-between mt-10 paragraph">
+            <div className="nav_left flex justify-between">
+              <span>Advik Aggrwal</span>
+              <span>Web Developer</span>
+            </div>
+            <div className="nav_right flex justify-between">
+              <span>Advik Aggrwal</span>
+              <span>Web Developer</span>
+            </div>
+          </nav>
+          <div>
+            <h1>FRONT END DEVELOPER</h1>
           </div>
-          <h1>FRONT END DEVELOPER</h1>
         </div>
       </section>
     </>
