6a9bc912426900618894d08a5412454c006306bd
anonymous-user02 <advikaggarwal02@gmail.com>
2024-04-01 13:10:47 +0530

    header edit

--- app/(main)/_components/Heroo.tsx
+++ app/(main)/_components/Heroo.tsx
@@ -1,13 +1,38 @@
-import Navbarc from "@/components/nav/Navbarc";
+"use client";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import dynamic from "next/dynamic";
+import LocomotiveScroll from "locomotive-scroll";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
+  const ref = useRef(null);
+  const advikRef = useRef(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      const scroll = new LocomotiveScroll({
+        el: ref.current,
+        smooth: true,
+      });
+
+      gsap.fromTo(
+        advikRef.current,
+        { y: "50%" },
+        { y: "0%", duration: 0.5, scrollTrigger: advikRef.current }
+      );
+    }
+  }, []);
+
   return (
-    <>
+    <div ref={ref} data-scroll-container>
       <section className="hero">
         <div className="h-screen w-screen   -mt-1  bg-[#111111] ">
           <nav className="flex justify-between mt-10 paragraph">
             <div className="nav_left flex justify-between mr-[50vh] open-font">
-              <span className="text-[#aaaaaa]">
+              <span className=" scroll text-[#aaaaaa]">
                 Advik <br /> Aggarwal
               </span>
               <span className="text-[#777777]">
@@ -24,12 +49,13 @@ const Hero = () => {
             </div>
           </nav>
           <div className=" w-screen h-screen flex items-center justify-center ml-10 tracking-[-0.5vh] ">
-            <h1 className="heading">FRONT ── END DEVELOPER</h1>
+            <h1 ref={advikRef} className=" heading">
+              FRONT ── END DEVELOPER
+            </h1>
           </div>
         </div>
       </section>
-    </>
+    </div>
   );
 };
-
 export default Hero;
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/nav/Navbar";
 import Image from "next/image";
 import Hero from "./_components/Hero";
+import Heroo from "./_components/Heroo";
+
 
 export default function Home() {
   return (
     <div className="  h-screen w-screen  bg-[#1e1e1e] items-end">
-      <Hero />{" "}
+      <Heroo />{" "}
     </div>
   );
 }
