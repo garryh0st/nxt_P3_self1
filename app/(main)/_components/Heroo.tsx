80db21a0a2f722ea74ed04eea59caacf1331d52c
anonymous-user02 <advikaggarwal02@gmail.com>
2024-04-01 14:32:49 +0530

    update

--- app/(main)/_components/Heroo.tsx
+++ app/(main)/_components/Heroo.tsx
@@ -12,6 +12,8 @@ const Hero = () => {
   const frontend = useRef(null);
   const developer = useRef(null);
   const navitems = useRef(null);
+  const frontendMaskRef = useRef(null);
+  const developerMaskRef = useRef(null);
 
   useEffect(() => {
     if (ref.current) {
@@ -21,32 +23,55 @@ const Hero = () => {
         direction: "horizontal",
       });
 
+      // gsap.fromTo(
+      //   frontend.current,
+      //   { y: "100%" },
+      //   { y: "0%", duration: 0.5, delay: 0.5, scrollTrigger: frontend.current }
+      // );
       gsap.fromTo(
         frontend.current,
-        { y: "50%" },
-        { y: "0%", duration: 0.5, delay: 0.5, scrollTrigger: frontend.current }
+        { y: "100%" },
+        { y: "0%", duration: 1, delay: 0.5, scrollTrigger: frontend.current }
       );
+
       gsap.fromTo(
         developer.current,
-        { y: "50%" },
-        { y: "0%", duration: 0.5, delay: 1, scrollTrigger: developer.current }
+        { y: "100%" },
+        { y: "0%", duration: 1, delay: 1, scrollTrigger: developer.current }
       );
-    }
-  }, []);
-
-  useEffect(() => {
-    if (frontend.current) {
       gsap.fromTo(
-        (frontend.current as HTMLElement).children,
-        { y: "40%", opacity: 0 },
-        {
-          y: "0%",
-          opacity: 1,
-          duration: 1,
-          stagger: 0.05,
-          scrollTrigger: frontend.current,
-        }
+        navitems.current,
+        { y: "-30vh" },
+        { y: "0%", duration: 3, scrollTrigger: navitems.current }
       );
+
+      const mask = frontend.current ? document.querySelector(".mask") : null;
+      gsap.to(mask, { y: "100%", duration: 1, ease: "power3.out" });
+      gsap.to(frontendMaskRef.current, {
+        y: "100%",
+        duration: 1,
+        ease: "power3.out",
+      });
+      gsap.to(developerMaskRef.current, {
+        y: "100%",
+        duration: 1,
+        ease: "power3.out",
+        delay: 1,
+      });
+
+      if (frontend.current) {
+        gsap.fromTo(
+          (frontend.current as HTMLElement).children,
+          { y: "40%", opacity: 0 },
+          {
+            y: "0%",
+            opacity: 1,
+            duration: 3,
+            stagger: 0.05,
+            scrollTrigger: frontend.current,
+          }
+        );
+      }
     }
   }, []);
 
@@ -54,7 +79,7 @@ const Hero = () => {
     <div ref={ref} data-scroll-container>
       <section className="hero">
         <div className="h-screen w-screen   -mt-1  bg-[#111111] ">
-          <nav className="flex justify-between mt-10 paragraph">
+          <nav ref={navitems} className="flex justify-between mt-10 paragraph">
             <div className="nav_left flex justify-between mr-[50vh] open-font">
               <span className=" scroll text-[#aaaaaa]">
                 Advik <br /> Aggarwal
@@ -72,17 +97,48 @@ const Hero = () => {
               <button className="btn-primary">CONTACT</button>
             </div>
           </nav>
-          <div className=" w-screen h-screen flex items-center justify-center ml-10 tracking-[-0.5vh] ">
-            <h1 ref={frontend} className=" heading">
-              {Array.from("FRONT ── END DEVELOPER").map((letter, index) => (
+          {/* <div className=" w-full h-screen flex items-center  ml-10 tracking-[-0.5vh] heading ">
+             {Array.from("FRONT ── END DEVELOPER").map((letter, index) => (
                 <span key={index} className="letter">
                   {letter}
                 </span>
-              ))}
-            </h1>
-          </div>
+              ))} 
+            <span ref={frontend}>Front --End</span>
+             {Array.from("FRONT ── END DEVELOPER").map((letter, index) => (
+                <span key={index} className="letter">
+                  {letter}
+                </span>
+              ))} 
+            <span ref={developer}>Developer</span>
+          </div> */}
+          <section className=" flex justify-center items-center h-screen w-screen ml-10 ">
+            <div className="w-full  ">
+              <div ref={frontend} className="heading tracking-[-0.2vh]">
+                <span className="txt-hover">F</span>
+                <span className="txt-hover">r</span>
+                <span className="txt-hover">o</span>
+                <span className="txt-hover">n</span>
+                <span className="txt-hover">t</span> ──{" "}
+                <span className="txt-hover">E</span>
+                <span className="txt-hover">n</span>
+                <span className="txt-hover">d</span>
+              </div>
+              <div ref={frontendMaskRef} className="mask"></div>
+              <div ref={developer} className="heading ">
+                <span className="txt-hover">D</span>
+                <span className="txt-hover">e</span>
+                <span className="txt-hover">v</span>
+                <span className="txt-hover">e</span>
+                <span className="txt-hover">l</span>
+                <span className="txt-hover">o</span>
+                <span className="txt-hover">p</span>
+                <span className="txt-hover">e</span>
+                <span className="txt-hover">r</span>
+              </div>
+              <div ref={developerMaskRef} className="mask"></div>
+            </div>
+          </section>
         </div>
-    
       </section>
     </div>
   );
